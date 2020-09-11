8b31d7f03a1518b8fa952c866402f14912516973
sourlemon <work_Xq@163.com>
2020-09-11 16:02:20 +0800

    test(constant):常量设置

---
+++ RocketMQ-Client/src/main/java/com/github/shaylau/rocketmq/demo/common/Constants.java
@@ -0,0 +1,14 @@
+package com.github.shaylau.rocketmq.demo.common;
+
+/**
+ * 常量
+ *
+ * @author ShayLau
+ * @date 2020/9/10 9:50 上午
+ */
+public class Constants {
+    /**
+     * NameSrv 服务地址
+     */
+    public static final String nameSrv = "localhost:9876";
+}
